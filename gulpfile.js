26579b33f150b665a00174854a376607926b7ae0
innab <innabraun08@gmail.com>
2021-03-10 21:30:39 +0100

    "new changes in gulpfile2"

--- gulpfile.js
+++ gulpfile.js
@@ -1,126 +1,106 @@
-const gulp = require('gulp');
-const uglify = require('gulp-uglify');
-const clean = require('gulp-clean-css');
+///requirements
+const gulp = require("gulp");
+const clean = require("gulp-clean");
+const browserSync = require("browser-sync").create();
+const cleanCSS = require("gulp-clean-css");
+const autoprefixer = require("gulp-autoprefixer");
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
+const imagemin = require("gulp-imagemin");
+
 const sass = require("gulp-sass");
-const concat = require('concat');
-const imagemin = require('imagemin');
-const watcher = require('watcher');
-const autoprefixer = require('gulp-autoprefixer');
-
 sass.compiler = require('node-sass');
-browserSync = require("browser-sync").create();
 
-//paths here
+const { src, dest, watch, parallel, series } = require("gulp");
+let sourceFolder="./dist";
+let devFolder="./src";
 
-const paths = {
+///paths
+
+const path = {
     html: "./index.html",
     src: {
-      scss: "./src/scss/**/*.scss",
-      js: "./src/js/*.js",
-      img: "./src/img/*",
+        scss: "./src/scss/**/*.scss",
+        js: "./src/js/*.js",
+        img: "./src/img/*",
     },
     dist: {
-      css: "./dist/css/",
-      js: "./dist/js/",
-      img: "./dist/img/",
-      self: "./dist/",
+        css: "./dist/css/",
+        js: "./dist/js/",
+        img: "./dist/img/",
+        self: "./dist/",
     },
-  };
-
-//functions here
-
-const cleanBuild = () => {
-  gulp.src(paths.dist.self, { allowEmpty: true }).pipe(clean());
-}
-
-
-
-
-const buildJS = () => {
-    gulp
-    .src(paths.src.js)
-    .pipe(concat("main.js"))
-    .pipe(gulp.dest(paths.dist.js))
-    .pipe(browserSync.stream());
+    watchers: {
+        scss: devFolder + "/scss/**/*.scss",
+    },
+    clean: sourceFolder,
 };
 
-const buildCSS = () => {
-    gulp
-    .src(paths.src.scss)
-    .pipe(sass().on("error", sass.logError))
-    .pipe(concat("main.css"))
-    .pipe(gulp.dest(paths.dist.css))
-    .pipe(browserSync.stream());
-};
+///functions
 
- const autoprefixerBuild = () => {
-    gulp
-    .src(paths.src.scss)
-    .pipe(autoprefixer({
-        cascade: false
-    }))
-    .pipe(gulp.dest('dist'))
- }
-
-const imageminBuild = () => {
-    gulp
-    .src(paths.src.img)
-    .pipe(imagemin())
-    .pipe(gulp.dest(paths.dist.img))
-};
-
-
-const buildIMG=()=>(
-    gulp.src(paths.src.img)
-        .pipe(imagemin())
-        .pipe(gulp.dest(paths.dist.img))
-        .pipe(browserSync.stream())
-)
-
-const uglifyBuild=()=>{
-    gulp.src('js/*.js','scss/*.css')
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js','dist/css'))
-}
-
-///dev tasks
-
-///Запуск сервера и последующее отслеживание изменений *.js и *.scss файлов в папке src;
-
-    const watcherBuild = () => {
-        browserSync.init({
+//watcher (обе dev задачи из тз)
+const watcher = () => {
+    browserSync.init({
         server: {
             baseDir: "./",
         },
+        port: 5000,
+        notify: false,
     });
-///При изменении - пересборка и копирование объединенных и минифицированных файлов styles.min.css и scripts.min.js в папку dist, перезагрузка вашей html-страницы.
-      
-      gulp.watch(paths.src.scss, buildCSS).on("change", browserSync.reload);
-      gulp.watch(paths.src.js, buildJS).on("change", browserSync.reload);
-      gulp.watch(paths.src.img, buildIMG).on("change", browserSync.reload);
-      gulp.watch(paths.html, build).on("change", browserSync.reload);
-      };
 
+    watch(path.watchers.scss, style).on("change", browserSync.reload);
+    watch(path.src.js, scripts).on("change", browserSync.reload);
+    watch(path.src.img, img).on("change", browserSync.reload);
+    watch("./*.html").on("change", browserSync.reload);
+};
 
-//tasks here: (задания для dev и build): (ШАБЛОН)
+///clean
+const cleanDist = () =>
+    src(path.clean, { allowEmpty: true }, { read: false }).pipe(clean());
 
-gulp.task("cleanBuild", cleanBuild);
-gulp.task("buildIMG", buildIMG);
-gulp.task("buildCSS", buildCSS);
-gulp.task("buildJS", buildJS);
-gulp.task("imageminBuild", imageminBuild);
-gulp.task("browser-sync", browserSync);
-gulp.task("autoprefixerBuild", autoprefixerBuild);
-gulp.task("watcherBuild", watcherBuild);
-gulp.task("uglifyBuild", uglifyBuild);
+///все задачи связанные с стилями
+const style = () =>
+    src(path.src.scss)
+        .pipe(concat("styles.min.css"))
+        .pipe(sass().on("error", sass.logError))
+        .pipe(
+            autoprefixer({
+                overrideBrowserslist: ["last 3 versions"],
+            })
+        )
+        .pipe(cleanCSS({ compatibility: "ie8" }))
+        .pipe(dest(path.dist.css))
+        .pipe(browserSync.stream());
+///все задачи связанные с скриптом
+const scripts = () =>
+    src(["src/js/script.js"])
+        .pipe(concat("scripts.min.js"))
+        .pipe(uglify())
+        .pipe(dest(path.dist.js))
+        .pipe(browserSync.stream());
 
-gulp.task("default", gulp.series(
-    cleanBuild,
-    buildCSS, 
-    buildJS,
-    imageminBuild,
-    browserSync,
-    watcherBuild,
-    autoprefixerBuild,
-    uglifyBuild
-));
+///все задачи связанные с картинками
+const img = () =>
+    src(path.src.img)
+        .pipe(
+            imagemin({
+                progressive: true,
+                svgoPlugins: [{ removeViewBox: false }],
+                interlaced: true,
+                optimizationLevel: 3,
+            })
+        )
+        .pipe(dest(path.dist.img))
+        .pipe(browserSync.stream());
+
+/// tasks
+exports.cleanDist = cleanDist;
+exports.style = style;
+exports.scripts = scripts;
+exports.img = img;
+exports.watcher = watcher;
+exports.build = series(
+    cleanDist,
+    parallel(style, scripts, img));
+exports.dev = watcher;
+exports.clean = cleanDist;
