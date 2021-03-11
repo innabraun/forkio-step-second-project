a9c56cb213e844f6b26adf813aaed03018b8fcda
innabraun <66827036+innabraun@users.noreply.github.com>
2021-03-11 20:01:17 +0100

    New header (#2)
    * new commit
    
    * "new changes in newHeader"
    
    * "new changes in newHeader2"
    
    * "new changes in newHeader3"
    
    * "new changes in newHea"
    
    * testik

--- gulpfile.js
+++ gulpfile.js
@@ -8,76 +8,54 @@ const uglify = require("gulp-uglify");
 const concat = require("gulp-concat");
 const imagemin = require("gulp-imagemin");
 
+
+const { src, dest, watch, parallel, series } = require("gulp");
+
+
+
 const sass = require("gulp-sass");
 sass.compiler = require('node-sass');
 
-const { src, dest, watch, parallel, series } = require("gulp");
 let sourceFolder="./dist";
 let devFolder="./src";
 
-///paths
+const path={
 
-const path = {
-    html: "./index.html",
-    src: {
-        scss: "./src/scss/**/*.scss",
-        js: "./src/js/*.js",
-        img: "./src/img/*",
+    dist:{
+        css:sourceFolder + "/css",
+        js:sourceFolder + "/js/",
+        img:sourceFolder + "/img/",
+
     },
-    dist: {
-        css: "./dist/css/",
-        js: "./dist/js/",
-        img: "./dist/img/",
-        self: "./dist/",
+
+    src:{
+        scss:devFolder + "/scss/**",
+        js: devFolder + "/js/*.js",
+        img: devFolder + "/img/**/*. {jpg,png,svg,gif,ico,webp}",
+
     },
-    watchers: {
+
+    watcher:{
         scss: devFolder + "/scss/**/*.scss",
     },
-    clean: sourceFolder,
+
+clean: sourceFolder,
+
 };
+
+
+
+
 
 ///functions
 
-//watcher (обе dev задачи из тз)
-const watcher = () => {
-    browserSync.init({
-        server: {
-            baseDir: "./",
-        },
-        port: 5000,
-        notify: false,
-    });
-
-    watch(path.watchers.scss, style).on("change", browserSync.reload);
-    watch(path.src.js, scripts).on("change", browserSync.reload);
-    watch(path.src.img, img).on("change", browserSync.reload);
-    watch("./*.html").on("change", browserSync.reload);
-};
-
-///clean
-const cleanDist = () =>
-    src(path.clean, { allowEmpty: true }, { read: false }).pipe(clean());
-
-///все задачи связанные с стилями
-const style = () =>
-    src(path.src.scss)
-        .pipe(concat("styles.min.css"))
-        .pipe(sass().on("error", sass.logError))
-        .pipe(
-            autoprefixer({
-                overrideBrowserslist: ["last 3 versions"],
-            })
-        )
-        .pipe(cleanCSS({ compatibility: "ie8" }))
-        .pipe(dest(path.dist.css))
-        .pipe(browserSync.stream());
 ///все задачи связанные с скриптом
 const scripts = () =>
-    src(["src/js/script.js"])
-        .pipe(concat("scripts.min.js"))
-        .pipe(uglify())
-        .pipe(dest(path.dist.js))
-        .pipe(browserSync.stream());
+    src(["src/js/main.js"])
+    .pipe(concat("scripts.min.js"))
+    .pipe(uglify())
+    .pipe(dest(path.dist.js))
+    .pipe(browserSync.stream());
 
 ///все задачи связанные с картинками
 const img = () =>
@@ -92,6 +70,52 @@ const img = () =>
         )
         .pipe(dest(path.dist.img))
         .pipe(browserSync.stream());
+//watcher (обе dev задачи из тз)
+const watcher = () => {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+        },
+        port: 5000,
+        notify: false,
+    });
+
+    watch(path.watcher.scss, style).on("change", browserSync.reload);
+    watch(path.src.js, scripts).on("change", browserSync.reload);
+    watch(path.src.img, img).on("change", browserSync.reload);
+    watch("./index.html").on("change", browserSync.reload);
+};
+
+///clean
+const cleanDist = () =>
+    src(path.clean, { allowEmpty: true }, { read: false }).pipe(clean());
+
+///все задачи связанные с стилями
+const style = () =>
+    // src(path.src.scss)
+src("src/scss/**")
+        .pipe(concat("styles.min.css"))
+        .pipe(sass().on("error", sass.logError))
+        .pipe(
+            autoprefixer({
+                overrideBrowserslist: ["last 3 versions"],
+            })
+        )
+        .pipe(cleanCSS({ compatibility: "ie8" }))
+        .pipe(dest(path.dist.css))
+        .pipe(browserSync.stream());
+
+
+
+
+// const build = () => {
+//     series(
+//         cleanDist,
+//         parallel(style, scripts, img));
+// }
+
+
+/// tasks
 
 /// tasks
 exports.cleanDist = cleanDist;
@@ -99,8 +123,10 @@ exports.style = style;
 exports.scripts = scripts;
 exports.img = img;
 exports.watcher = watcher;
-exports.build = series(
-    cleanDist,
-    parallel(style, scripts, img));
-exports.dev = watcher;
-exports.clean = cleanDist;
+// exports.build = series(
+//     cleanDist,
+//     parallel(style, scripts, img));
+// exports.dev = watcher;
+// exports.build = build;
+exports.default = series(series(cleanDist, parallel(style, scripts, img)),watcher  )
+
